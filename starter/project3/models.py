925f53b46d27059100944a7a525e5a4ba4a9f513
Nsikak Thomas <nsikakthomas102@gmail.com>
2024-05-20 08:34:49 +0100

    sqlalchemy package added

--- starter/project3/models.py
+++ starter/project3/models.py
@@ -1,7 +1,8 @@
 from database import Base
 from sqlalchemy import Column, Integer
 
-class Todos(Base):
-  __tablename__ = "todos"
 
-  # id = Column(Integer, primary_key=True, inde)
+class Todos(Base):
+    __tablename__ = "todos"
+
+    # id = Column(Integer, primary_key=True, index)
