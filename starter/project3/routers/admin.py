f0a6e8c462ee8fd1c899eb38514bd7106b66b165
Nsikak Thomas <nsikakthomas102@gmail.com>
2024-05-28 08:33:59 +0100

    admin functions complete

--- starter/project3/routers/admin.py
+++ starter/project3/routers/admin.py
@@ -16,7 +16,22 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @router.get("/todos", status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
-    if user is None or user.get("role") is not "admin":
+    if user is None or user.get("role") != "admin":
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication failed")
     return db.query(Todos).all()
+
+
+@router.delete("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int):
+    if user is None or user.get("role") != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+    todo_record = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_record is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Todo not found"
+        )
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
